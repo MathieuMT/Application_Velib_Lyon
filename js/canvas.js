cf13e056a33b7d0f2c52cab808733cafcd72e976
2mmtmc@gmail.com <mathieudemilloterrazzani@iMac-de-Mathieu-2.local>
2020-03-10 00:46:46 +0100

    refonte graphique, mise en poo le JS et  modifications avec W3C validator

--- js/canvas.js
+++ js/canvas.js
@@ -1,159 +1,144 @@
-// OBJET Canvas:
+/* on définit la classe Canvas */ 
+class Canvas{
+	constructor(){
+		this.canvas = document.querySelector("canvas");
+		this.cleanCanvas = document.getElementById("btn_effacer");
+		this.ctx = this.canvas.getContext("2d");
+		this.canvas.width = 310;
+		this.canvas.height = 140;
+		this.canvasBlank = true;
+		this.painting = false;
+		this.controlMouse();
+        this.controlTouch();
+		this.btnCleaning();
+		this.cleaning();
+     }
+ 
+	startPosition()  {
+        let Canvas = this;
+		Canvas.painting = true;  
+		Canvas.canvasBlank = false;
+		console.log(Canvas); 
+	 }
+ 
+	finishedPosition() {
+		let Canvas = this;
+        Canvas.painting = false;
+	    Canvas.ctx.beginPath();
+	}
 
-var Canvas = {
-    init: function() {
-        this.mouseX;
-        this.mouseY;
-        this.mouseDown = 0;
-        
-        this.touchX;
-        this.touchY;
-        
-        this.radius = 2;
-        
-        this.canvas = document.getElementById('canvas');
-        
-        this.ctx = this.canvas.getContext('2d');
-        
-        
-    
-    
-        // Évènements souris:
-        
-        this.canvas.addEventListener('mousedown', this.sketchpad_mouseDown, false);
-        
-        this.canvas.addEventListener('mousemove', this.sketchpad_mouseMove, false);
-        
-        this.canvas.addEventListener('mouseup', this.sketchpad_mouseUp, false);
-        
-        
-        // Évènements tactil:
-        
-        this.canvas.addEventListener('touchstart', this.sketchpad_touchStart, false);
-        
-        this.canvas.addEventListener('touchmove', this.sketchpad_touchMove, false);
-        
-        this.canvas.addEventListener('touchend', this.skechpad_touchEnd, false);
-    
-     },
-        
-    drawLine: function (ctx, x, y, size) {
-        // Si lastX n'est pas défini, définissez lastX et lastY sur la position actuelle 
-        if (Canvas.lastX == -1) {
-            Canvas.lastX = x;
-            Canvas.lastY = y;
-        }
-    
-        // Utilisons le noir en réglant les valeurs RVB sur 0 et 255 alpha (complètement opaque)
-        r=0; g=0; b=0; a=255;
-    
-        // Sélectionnez un style de remplissage
-        Canvas.ctx.strokeStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
-    
-        // Réglez le style de la ligne "cap" sur "round", afin que les lignes à différents angles puissent se rejoindre
+	draw(e) {
+		let Canvas = this;
+		if (!Canvas.painting) return;
+        Canvas.ctx.lineWidth = 2;
         Canvas.ctx.lineCap = "round";
-    
-        // Dessiner une ligne remplie
-        Canvas.ctx.beginPath();
-    
-        // D'abord, passez à l'ancienne position (précédente)
-        Canvas.ctx.moveTo(Canvas.lastX, Canvas.lastY);
-    
-        // Dessinez maintenant une ligne jusqu'à la position actuelle du pointeur
-        Canvas.ctx.lineTo(x,y);
-    
-        // Définir l'épaisseur de la ligne et dessiner la ligne
-        Canvas.ctx.lineWidth = size;
+        Canvas.ctx.lineTo(e.offsetX, e.offsetY); 
         Canvas.ctx.stroke();
+        Canvas.ctx.beginPath();
+        Canvas.ctx.moveTo(e.offsetX, e.offsetY);
+        Canvas.ctx.strokeStyle = "#007bff";
+	}
+ 
+	 cleaning() {
+		let Canvas = this;
+		Canvas.ctx.clearRect(0, 0, 310, 140);
+		Canvas.canvasBlank = true;
+     }
+ 
     
-        Canvas.ctx.closePath();
-    
-        // Mettre à jour la dernière position pour référencer la position actuelle
-        Canvas.lastX = x;
-        Canvas.lastY = y;
-    
-    },
-    
-    effacerSignature: function(){
-        Canvas.ctx.clearRect(0, 0, Canvas.canvas.width, Canvas.canvas.height);
-    },
-    
-    sketchpad_mouseDown: function () {
-        Canvas.mouseDown = 1;
-        Canvas.drawLine(Canvas.ctx, Canvas.mouseX, Canvas.mouseY, Canvas.radius);
-    },
-    
-    sketchpad_mouseUp: function () {
-        Canvas.mouseDown = 0;
-        
-        Canvas.lastX = -1;
-        Canvas.lastY = -1;
-    },
-    
-    sketchpad_mouseMove: function (e) {
-        Canvas.getMousePos(e);
-        
-        if (Canvas.mouseDown == 1) {
-            Canvas.drawLine (Canvas.ctx, Canvas.mouseX, Canvas.mouseY, Canvas.radius);
-        }
-    },
-    
-    getMousePos: function (e) {
-        if (!e) {
-            var e = event;
-        }
-    
-    if (e.offsetX) {
-            Canvas.mouseX = e.offsetX;
-            Canvas.mouseY = e.offsetY;
-        }
-    
-    else if (e.layerX) {
-            Canvas.mouseX = e.layerX;
-            Canvas.mouseY = e.layerY;
-        }
-    },
-    
-    sketchpad_touchStart: function (e) {
-        Canvas.getTouchPos (e);
-        Canvas.drawLine(Canvas.ctx, Canvas.touchX, Canvas.touchY, Canvas.radius);
-        
-        e.preventDefault();
-    },
-    
-    skechpad_touchEnd: function () {
-        Canvas.lastX = -1;
-        Canvas.lastY = -1;
-    },
-    
-    sketchpad_touchMove: function (e) {
-        Canvas.getTouchPos (e);
-        
-        Canvas.drawLine (Canvas.ctx, Canvas.touchX, Canvas.touchY, Canvas.radius);
-        
-        e.preventDefault();
-    },
-    
-    getTouchPos: function (e) {
-        if (!e) 
-        var e = event;
-   
-    
-        if (e.touches) {
-            if (e.touches.length == 1) {// Ne traiter qu'avec un doigt
-            
-                var touch = e.touches[0]; // Obtenez l'information pour le doigt # 1
-                Canvas.touchX = touch.pageX-touch.target.offsetLeft;
-                Canvas.touchY = touch.pageY-touch.target.offsetTop;
-            }
-        }
-    },
-    
-};
+    /* Obtenir la position d'une touche par rapport à la toile */
+    getTouchPos(canvasDom, touchEvent) {
+        var rect = canvasDom.getBoundingClientRect();
+        return {
+            x: touchEvent.touches[0].clientX - rect.left,
+            y: touchEvent.touches[0].clientY - rect.top
+        };
+    }
 
+ 
+    /* FONCTION POUR DESSINER AVEC LA SOURIS */
+    controlMouse(){
+        let Canvas = this;
+ 
+        Canvas.canvas.addEventListener("mousedown", function(){
+			 Canvas.startPosition();
+		});
+ 
+		Canvas.canvas.addEventListener("mousemove", function (e) {
+		        Canvas.draw(e);
+		});
+ 
+        Canvas.canvas.addEventListener("mouseup", function(){
+			Canvas.finishedPosition();
+		});
+ 
+		Canvas.canvas.addEventListener("mouseleave", function(){
+			Canvas.finishedPosition();
+		});
+	}
+    
+    /* POUR DESSINER SUR ECRAN TACTILE */
+    controlTouch() {
+        
+        let Canvas = this;
+        
+        let mousePos = { x:0, y:0 };
+        
+		 Canvas.canvas.addEventListener("touchstart", function (e) {
+            mousePos = Canvas.getTouchPos(Canvas.canvas, e);
+            let touch = e.touches[0];
+            let mouseEvent = new MouseEvent("mousedown", {
+                clientX: touch.clientX,
+                clientY: touch.clientY
+            });
+            Canvas.canvas.dispatchEvent(mouseEvent);
+        }, false);
+ 
+		Canvas.canvas.addEventListener("touchmove", function (e) {
+            let touch = e.touches[0];
+            let mouseEvent = new MouseEvent("mousemove", {
+                clientX: touch.clientX,
+                clientY: touch.clientY
+            });
+            Canvas.canvas.dispatchEvent(mouseEvent);
+        }, false);
+ 
+		 Canvas.canvas.addEventListener("touchend", function (e) {
+            let mouseEvent = new MouseEvent("mouseup", {});
+            Canvas.canvas.dispatchEvent(mouseEvent);
+        }, false);
+        
+        
+        
+        /* Empêcher le défilement en touchant la toile */
+        document.body.addEventListener("touchstart", function (e) {
+          if (e.target == Canvas.canvas) {
+            e.preventDefault();
+          }
+        }, false);
+        
+        document.body.addEventListener("touchend", function (e) {
+          if (e.target == Canvas.canvas) {
+            e.preventDefault();
+          }
+        }, false);
+        
+        document.body.addEventListener("touchmove", function (e) {
+          if (e.target == Canvas.canvas) {
+            e.preventDefault();
+          }
+        }, false);   
+    }
+ 
+    /* fonction du bouton effacer la toile */
+	btnCleaning(){
+		let Canvas = this;
+		Canvas.cleanCanvas.onclick = function (){ 
+			Canvas.cleaning();
+	         }
+	}
+ 
+}  
 
-window.addEventListener('load', function(){
-    
-    Canvas.init();
-    
-} );
+/* c est l'objet instancié de Canvas */
+let c = new Canvas();
